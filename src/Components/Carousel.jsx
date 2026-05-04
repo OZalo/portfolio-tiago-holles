3f29ea8f4e059c490aeaa7a30eb7518df5170c4c
OZalo <renansabat@gmail.com>
2026-05-04 19:50:04 -0300

    refactor: hotfix

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -125,6 +125,7 @@ const Carousel = () => {
             }}>
               <div style={{ position: 'relative', paddingTop: '56.25%', backgroundColor: '#000', borderRadius: '8px', overflow: 'hidden' }}>
                 <iframe 
+                  title="Vídeo do Projeto"
                   src={selectedContent.video || selectedContent.upperVideo} 
                   style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                   frameBorder="0" 
